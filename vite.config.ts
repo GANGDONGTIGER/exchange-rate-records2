02f9016fb3e826843f009cf9f9aa6e71ff715b6b
luddite <sun22002@gmail.com>
2026-04-22 12:22:59 +0900

    fix_chunkSizeWarning 스레쉬홀드 수치 1500 증가

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/exchange-rate-records2/' // GitHub Pages에 배포할 때 필요한 설정
+  base: '/exchange-rate-records2/', // GitHub Pages에 배포할 때 필요한 설정
+  build: {
+    chunkSizeWarningLimit : 1500
+  }
 })
